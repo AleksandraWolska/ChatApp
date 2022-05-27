6b2278a1aa32bd75c80a2c2f91bcd70e9c367a59
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2022-05-27 10:47:04 +0200

    chatroom changed

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ from flask_socketio import SocketIO, join_room, leave_room, emit
 from flask_session import Session
 
 app = Flask(__name__)
+
 app.debug = True
 app.config['SECRET_TYPE'] = 'secret'
 app.config['SESSION_TYPE'] = 'filesystem'
 
 
+messageList = []
+matchesList = []
+
+
 Session(app)
 
-socketio = SocketIO(app, manage_session=False)
+socketio = SocketIO(app,always_connect=True, engineio_logger=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -43,16 +48,48 @@ def messenger():
 
 
 
+
+@socketio.on('connect')
+def connected():
+    print('connect')
+    
+@socketio.on('disconnect')
+def disconnect():
+    print('disconnect')
+
+
+
 @socketio.on('join', namespace='/chatroom')
 def join(message):
     room = session.get('room')
     join_room(room)
-    emit('status', {'msg': session.get('name') + ' entered the room.'}, room=room)
+    emit('status', {'msg': session.get('name') + ' entered the room.', 'author': session.get('name')}, room=room)
+
+
 
 @socketio.on('text', namespace='/chatroom')
 def text(message):
+    if (not message['msg']): 
+        return
     room = session.get('room')  
-    emit('message', {'msg': message['msg'], 'author': session.get('name')}, room=room)
+    newMessage = {'msg': message['msg'], 'author': session.get('name')}
+    messageList.append(newMessage)
+    print(messageList)
+    emit('message', newMessage, room=room)
+
+
+
+@socketio.on('image-upload', namespace='/chatroom')
+def imageUpload(image):
+    room= session.get('room')
+    emit('send-image', {'msg': image, 'author': session.get('name')}, room=room)
+
+# @socketio.on('image-upload')
+# def imageUpload(image):
+#     emit('send-image', image, broadcast = True)
+
+
+
 
 @socketio.on('left', namespace='/chatroom')
 def left(message):
@@ -61,19 +98,18 @@ def left(message):
     name = session.get('name')
     leave_room(room)
     session.clear()
-    emit('status', {'msg': name + ' has left the room'}, room=room)
+    emit('status', {'msg': name + ' has left the room', 'author': session.get('name')}, room=room)
+
 
 
 
 @socketio.on('message')
-def handleMessage():
-    pass
-
-
-
-
+def handleMessage(data):
+    print('received message: ' + data)
+    
 
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    
+    socketio.run(app, debug=True)
